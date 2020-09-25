a0fd536bc2902639778dc3823e89a55de7a6ddd5
Dylan STIENNE <dylan@macbook-pro-de-dylan.home>
2020-09-25 12:52:56 +0200

    save code

---
+++ public/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <a href="#" class="item">
+        <p>Accueil</p>
+    </a>
+    <a href="#" class="item">
+        <p>Mentions legales</p>
+    </a>
+    <a href="#" class="item">
+        <p>Contact</p>
+    </a>
+    <a href="#" class="item">
+        <p>Connexion</p>
+    </a>
+</footer>
---
+++ public/header.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <a href="#" id="logo" class="item">
+            <img src="img/poll-solid.svg" alt="Survey Game">
+            <h1>Survey Game</h1>
+        </a>
+        <div class="item" id="search">
+            <input type="search" placeholder="Rechercher un sondage" name="search">
+        </div>
+        <a href="#" class="item">
+            <img src="img/user-solid.svg" alt="Connexion">
+            <p>Connexion</p>
+        </a>
+    </nav>
+</header>
